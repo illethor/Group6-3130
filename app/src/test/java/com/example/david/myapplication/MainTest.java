bfc7279dc6f81b75ef79ce6f24907e13e7ef0e11
illethor <davidmongrain221@gmail.com>
2017-06-01 12:43:33 -0300

    Added button tests

---
+++ app/src/test/java/com/example/david/myapplication/MainTest.java
@@ -0,0 +1,40 @@
+package com.example.david.myapplication;
+
+import org.junit.Test;
+
+/**
+ * Created by David on 2017-06-01.
+ */
+
+public class MainTest {
+
+    @Test
+    public void button1Test() throws Exception{
+        //test here
+    }
+
+    @Test
+    public void button2Test() throws Exception{
+        //test here
+    }
+
+    @Test
+    public void button3Test() throws Exception{
+        //test here
+    }
+
+    @Test
+    public void button4Test() throws Exception{
+        //test here
+    }
+
+    @Test
+    public void button5Test() throws Exception{
+        //test here
+    }
+
+    @Test
+    public void button6Test() throws Exception{
+        //test here
+    }
+}
