d634ce3bc3930c6956d492bb3c1e295644c38ceb
pachalB <broganpachal@gmail.com>
2017-06-08 12:14:45 -0300

    Simple UI with simple tests (BP, DMc)

---
+++ app/src/test/java/com/example/david/myapplication/SetRangesTest.java
@@ -0,0 +1,32 @@
+package com.example.david.myapplication;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by broganpachal on 2017-06-06.
+ */
+
+public class SetRangesTest {
+    @Test
+    public void spnActivitiesTest() throws Exception{
+        assertTrue(false);
+    }
+    @Test
+    public void spnInfoTest() throws Exception{
+        assertTrue(false);
+    }
+    @Test
+    public void etLowerBoundTest() throws Exception{
+        assertTrue(false);
+    }
+    @Test
+    public void etUpperBoundTest() throws Exception{
+        assertTrue(false);
+    }
+    @Test
+    public void btnApplyTest() throws Exception{
+        assertTrue(false);
+    }
+}
